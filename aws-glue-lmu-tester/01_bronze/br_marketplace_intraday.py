361574ec6e7abc1f8ebf1fa72d0ec07400740f1d
Laurent Martineau <l.martineau971@gmail.com>
2023-08-23 21:24:02 +0200

    Add job extraction from marketstack

--- aws-glue-lmu-tester/01_bronze/br_marketplace_intraday.py
+++ aws-glue-lmu-tester/01_bronze/br_marketplace_intraday.py
@@ -1,8 +1,10 @@
 import boto3
 import requests
+import json
 
 
 client_ssm = boto3.client('ssm')
+client_s3 = boto3.client('s3')
 
 response = client_ssm.get_parameters(
     Names=[
@@ -10,8 +12,17 @@ response = client_ssm.get_parameters(
     ]
 )
 
-api_keys = response['Parameters'][0]['Value']
+api_key = response['Parameters'][0]['Value']
 
-# api_result = requests.get('https://api.marketstack.com/v1/tickers/aapl/eod', params)
+api_call = requests.get(f"""http://api.marketstack.com/v1/eod?access_key={api_key}&symbols=AAPL& date_from=2023-08-10&date_to=2023-08-20""")
 
-# api_response = api_result.json()
+response = api_call.json()
+
+client_s3.put_object(
+    Bucket='lmu-data',
+    Key='bronze/marketstack/eod/Symbol=AAPL/from_2023_08_10_to_2023_08_20.json',
+    Body=json.dumps(response, indent=2, default=str)
+)
+
+
+
